8ce2702a495702c09cb6573d3934143b16250a06
Meron Seyoum <meryato@gmail.com>
2024-10-28 22:36:10 -0600

    layout update to home page brand and three banner section

--- components/BrandCard.js
+++ components/BrandCard.js
@@ -1,47 +1,60 @@
-import React from 'react';
-import Image from 'next/image';
-import { MdStar } from 'react-icons/md';
-import { PiSealCheckFill } from 'react-icons/pi';
-import ButtonSecondary from 'shared/Button/ButtonSecondary';
-import apple from 'images/apple.png';
+import React from "react";
+import Image from "next/image";
+import { MdStar } from "react-icons/md";
+import { PiSealCheckFill } from "react-icons/pi";
+import ButtonSecondary from "shared/Button/ButtonSecondary";
+import apple from "images/apple.png";
 
 const BrandCard = ({
   brandName,
   rating = 4.5,
   reviews = 10,
-  followers = '2 M',
+  followers = "2 M",
   visitLink,
   logo = apple,
   products,
 }) => {
   return (
-    <div className="rounded-2xl border border-neutral-300 p-6 ">
+    <div className="rounded-2xl border border-neutral-300 p-3">
       <div className="flex items-center justify-between">
-        <div className="flex items-center gap-3">
-          <div className="w-16 h-16 overflow-hidden rounded-lg bg-white flex items-center justify-center">
-            <Image src={logo} alt={`${brandName} logo`} className="object-contain" />
-          </div>
-          <div>
-            <h3 className="flex items-center gap-1 text-base font-medium">
+        <div className="flex-col items-center ">
+          <div className= " overflow-hidden rounded-lg bg-white flex space-x-1 items-center justify-center">
+            <Image
+              src={logo}
+              alt={`${brandName}`}
+              className="object-contain w-10 h-10"
+              width={100}
+              height={10}
+            />
+            <h3 className="flex items-center gap-1 text-base sm:text-sm font-medium">
               {brandName} <PiSealCheckFill className="text-blue-600" />
             </h3>
-            <div className="flex items-center gap-1 text-sm text-neutral-500">
+          </div>
+
+          
+            <div className="flex items-center gap-2 ml-10 text-sm text-neutral-500">
               <MdStar className="text-yellow-400" />
               <span>{rating}</span>
               <span>{`(${reviews} Reviews)`}</span>
             </div>
-            <p className="text-sm text-neutral-500">{`${followers} Followers`}</p>
-          </div>
+            <p className="text-sm text-neutral-500 ml-11">{`${followers} Followers`}</p>
+          
         </div>
 
-        <ButtonSecondary to={visitLink} className="border border-black px-4 py-2">
+        <ButtonSecondary
+          to={visitLink}
+          className="border border-black px-4 py-2"
+        >
           Visit
         </ButtonSecondary>
       </div>
 
       <div className="mt-6 grid grid-cols-2 gap-4">
         {products.map((product, index) => (
-          <div key={index} className="h-[120px] overflow-hidden rounded-lg border p-2 bg-neutral-light">
+          <div
+            key={index}
+            className="h-[120px] overflow-hidden rounded-lg border p-2 bg-neutral-light"
+          >
             <Image
               src={product.main_image}
               alt={`${product.name} image`}
--- pages/home/SectionCmsBannerThree.js
+++ pages/home/SectionCmsBannerThree.js
@@ -16,7 +16,7 @@ const SectionCmsBannerThree = ({banners}) => {
   return (
     <section className="flex flex-wrap lg:flex-row flex-col mb-10 gap-5">
       {banners && banners.map((banner) => (
-        <div key={banner.id} className="w-full sm:w-[48%] md:w-[32.1%] ">
+        <div key={banner.id} className="w-full sm:w-[48%] md:w-[32%] ">
           <div className="relative rounded-md overflow-hidden">
             <Image 
               src={banner.image}
@@ -26,11 +26,11 @@ const SectionCmsBannerThree = ({banners}) => {
               className="object-cover rounded-md w-full h-auto"
             />
             <div className="absolute inset-0 bg-black bg-opacity-20 flex flex-col justify-center lg:px-8 px-4">
-              <div className='flex lg:gap-3 gap-2 flex-col w-full lg:w-[60%]'>
-                <h2 className="lg:text-2xl text-lg font-semibold text-white">{banner.title}</h2>
+              <div className='flex lg:gap-3 gap-2 flex-col w-full lg:w-[70%] '>
+                <h2 className=" banner-title font-semibold text-white">{banner.title}</h2>
                 <span className="text-sm text-white">
                   From &nbsp;
-                  <span className="font-semibold text-2xl text-primary-light">{banner.price}</span>
+                  <span className="font-semibold  banner-price text-primary-light">{banner.price}</span>
                 </span>
                 <div className="mt-4">
                 <Link href={banner.link}>
